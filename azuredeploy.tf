82b569b6f6b4a1dc3c611d9a59b99e067f946cbd
rsmithtamu <68651059+rsmithtamu@users.noreply.github.com>
2021-07-27 11:29:04 -0500

    Updates

--- azuredeploy.tf
+++ azuredeploy.tf
@@ -11,7 +11,7 @@ resource "azurerm_virtual_network" "region1-vnet1-hub1" {
   location            = var.loc1
   resource_group_name = azurerm_resource_group.rg1.name
   address_space       = [var.region1-vnet1-address-space]
-  dns_servers         = ["10.10.1.4", "168.63.129.16", "8.8.8.8"]
+  dns_servers         = ["8.8.4.4", "8.8.8.8"]
 
 }
 resource "azurerm_subnet" "region1-vnet1-snet1" {
